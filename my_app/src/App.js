511535e271a737b7fc86916a8ec6953d610c6c38
Shwetanshu89 <shekharahwet45@gmail.com>
2023-06-25 22:30:00 +0530

    added app

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const [searchName, setSearchName] = useState('');
+
+  useEffect(() => {
+    empData();
+  }, []);
+
+  const empData = async () => {
+    try {
+      const response = await axios.get('https://reqres.in/api/users?page=2');
+
+     
+      setUsers(response.data.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  const handleSearchChange = (event) => {
+    setSearchName(event.target.value);
+  };
+
+  const filteredUsers = users.filter((user) =>
+    user.first_name.toLowerCase().includes(searchName.toLowerCase())
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1>Employee List</h1>
+      <input
+        type="text"
+        value={searchName}
+        onChange={handleSearchChange}
+        placeholder="Enter name"
+      />
+      {filteredUsers.map((user) => (
+           <div >
+        <div key={user.id} className='employee-card' >
+         
+          <img src={user.avatar} alt="User" />
+          <p>{user.id}</p>
+          </div>
+          <p> {user.first_name}</p>
+        
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
